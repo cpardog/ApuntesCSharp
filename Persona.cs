3e7b9d9a738aaebdeb27d48098475013041c0c4d
Charlie <55631934+cpardog@users.noreply.github.com>
2021-08-24 20:33:07 -0500

    Add files via upload

---
+++ Persona.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primos
+{
+    class Persona
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime Fechanac { get; set; }
+        public long Id_persona { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i = 1; string cadena;
+            int contaprimos = 0; int secuencia = 1;
+            Console.WriteLine("Escriba Cuantos primos desea");
+            cadena = Console.ReadLine();
+            i = Convert.ToInt32(cadena);
+            while (contaprimos <i)
+            {
+             if (esprimo(secuencia))
+                        {
+                            Console.WriteLine((contaprimos+1).ToString() +"- " + secuencia+ "el numero es primo");
+                            Console.WriteLine("Que bonita es esta linea");
+                    contaprimos++;
+                        }
+                //else
+                //{
+                //    Console.WriteLine(i+ "El numero no es primo");
+                //}
+                secuencia++;
+            }
+           
+            Console.ReadLine();
+            int j = 10;
+            while (j<100)
+            {
+                Console.WriteLine("Ayudame a escribir codigo correcto!");
+                j++;
+            }
+        }
+
+        public static bool esprimo(int numero)
+        {
+            int ciclo = 1;
+            
+            int divisores=0 ;
+            while (ciclo<=numero)
+            {
+                if (numero % ciclo==0)
+                {
+                    divisores++;
+                }
+                ciclo++;
+            }
+            if (divisores <= 2) return true;
+            else  return false;
+        }
+    }
+}
